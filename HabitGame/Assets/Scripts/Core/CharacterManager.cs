859d72fce6c760555b8dc0a23b84bc7be2f5ebb8
Bam2 <1004jangsan@gmail.com>
2026-06-07 20:27:27 +0900

    set CharacterManager

---
+++ HabitGame/Assets/Scripts/Core/CharacterManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : Singleton<CharacterManager>
+{
+    // 캐릭터 최신 정보
+    // 외부에서 직접적으로 참조할 일이 없다면 private으로 잠구는게 낫긴 함
+    public CharacterResponse characterStatusData { get; private set; }
+
+    // 캐릭터 최신 정보 업데이트
+    // 캐릭터 정보에 대한 수정 발생 시(생활 습관 목표 보상 수령) 받아오는 응답데이터에서 CharacterResponse를 추출해서 호출
+    // 항상 최신 상태를 유지하도록
+    public void UpdateCharacterStatus(CharacterResponse data)
+    {
+        characterStatusData = data;
+    }
+
+    // 전투 시 필요한  my BattleUnit 생성 
+    // 전투에 사용할 특성 선택이 반영되므로 선택 이후에 호출하도록
+    public BattleUnit CreateBattleUnit(AttributeType selectedAttr)
+    {
+        int attrLevel = selectedAttr switch
+        {
+            AttributeType.Fire => characterStatusData.FireLv,
+            AttributeType.Water => characterStatusData.WaterLv,
+            AttributeType.Grass => characterStatusData.GrassLv,
+            AttributeType.Aurora => characterStatusData.AuroraLv
+        };
+
+
+        return new BattleUnit
+        {
+            name = "Name",    // 이름 어디서 끌어오지 << 어디 저장하지? 이름?????
+            hp = characterStatusData.Hp,    
+            atk = (int)characterStatusData.Atk,    // 타입 통일 필요 (float or int)
+            def = (int)characterStatusData.Def,
+            spd = (int)characterStatusData.Spd,
+            crtk = characterStatusData.Crit,
+            attribute = selectedAttr,
+            attributeLevel = attrLevel
+        };
+    }
+}
