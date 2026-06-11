321af72624d584d60c46bbc0b84ff6888ef5ff32
Bam2 <1004jangsan@gmail.com>
2026-06-11 19:37:41 +0900

    modify BattleUI test

--- HabitGame/Assets/Scripts/PvP/BattleTest.cs
+++ HabitGame/Assets/Scripts/PvP/BattleTest.cs
@@ -7,8 +7,8 @@ public class BattleTest : MonoBehaviour
     [SerializeField]
     BattleManager battleManger;
 
-    BattleUnit my = new BattleUnit { name = "my", atk = 10, attribute = AttributeType.Fire, attributeLevel = 3, crtk = 0f, def = 4, hp = 400, spd = 4, maxHp = 400 };
-    BattleUnit opp = new BattleUnit { name = "opp", atk = 10, attribute = AttributeType.Fire, attributeLevel = 3, crtk = 0f, def = 4, hp = 400, spd = 3, maxHp = 400 };
+    BattleUnit my = new BattleUnit { name = "my", atk = 20, attribute = AttributeType.Water, attributeLevel = 3, crtk = 0.3f, def = 4, hp = 500, spd = 4, maxHp = 500 };
+    BattleUnit opp = new BattleUnit { name = "opp", atk = 10, attribute = AttributeType.Fire, attributeLevel = 3, crtk = 0.1f, def = 3, hp = 400, spd = 3, maxHp = 400 };
 
     private void Awake()
     {
@@ -23,5 +23,11 @@ public class BattleTest : MonoBehaviour
         battleManger.StartBattle(my, opp);
         BattleUIManager.Instance.GetName(my.name, opp.name);
         BattleUIManager.Instance.InitBattleUI();
+        BattleUIManager.Instance.ReadyComplete();
+    }
+
+    public void LoadingComplete()
+    {
+        BattleUIManager.Instance.LoadingComplete();
     }
 }
