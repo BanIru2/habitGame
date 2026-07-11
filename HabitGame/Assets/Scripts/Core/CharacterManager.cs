42626986f68949a77777ed64f761764481129576
Bam2 <1004jangsan@gmail.com>
2026-07-11 18:07:20 +0900

    CharacterManager refactoring : add async character sync flow
    DB 접근을 위한 비동기 함수에 대해 CharacterResponse 흐름 구조를 일관적으로 만들기 위한 리팩토링 (+ 비동기 함수 추가)

--- HabitGame/Assets/Scripts/Core/CharacterManager.cs
+++ HabitGame/Assets/Scripts/Core/CharacterManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class CharacterManager : Singleton<CharacterManager>
@@ -7,6 +8,9 @@ public class CharacterManager : Singleton<CharacterManager>
     // 캐릭터 최신 정보
     // 외부에서 직접적으로 참조할 일이 없다면 private으로 잠구는게 낫긴 함
     public CharacterResponse characterStatusData { get; private set; }
+
+    // 캐릭터 닉네임
+    private string nickname = "Unknown";
 
     // 착용한 아이템 정보
     private List<EquipmentDataSO> equippedItems = new List<EquipmentDataSO>();
@@ -16,13 +20,23 @@ public class CharacterManager : Singleton<CharacterManager>
     {
         base.Awake();
         // 임시 데이터
-        UpdateCharacterStatus(new CharacterResponse { UserId = 123, Atk = 10, Def = 50, Hp = 1000, Spd = 10, FireLv = 5, WaterLv = 3, GrassLv = 1, AuroraLv = 0 }); 
+        ApplyCharacterResponse(new CharacterResponse { UserId = 123, Atk = 10, Def = 50, Hp = 1000, Spd = 10, FireLv = 5, WaterLv = 3, GrassLv = 1, AuroraLv = 0 });
+    }
+
+    // CharacterREsponse를 요청하는 비동기함수
+    public async Task<CharacterResponse> RefreshCharacterAsync()
+    {
+        CharacterResponse response = await ServiceRegistry.Instance.Character.GetMyCharacterAsync();
+
+        ApplyCharacterResponse(response);
+
+        return response;
     }
 
     // 캐릭터 최신 정보 업데이트
-    // 캐릭터 정보에 대한 수정 발생 시(생활 습관 목표 보상 수령) 받아오는 응답데이터에서 CharacterResponse를 추 출해서 호출
-    // 항상 최신 상태를 유지하도록
-    public void UpdateCharacterStatus(CharacterResponse data)
+    // 캐릭터 정보에 대한 수정 발생 시(생활 습관 목표 보상 수령) 받아오는 응답데이터에서 CharacterResponse를 추출해서 호출
+    // 단순 조회 시에는 RefreshCharacterAsync()에서 호출되어 DB와 동기화시키는 목적
+    public void ApplyCharacterResponse(CharacterResponse data)
     {
         characterStatusData = data;
     }
@@ -38,24 +52,28 @@ public class CharacterManager : Singleton<CharacterManager>
             case StatType.CRIT: crtk += bonus.value; break;
         }
     }
+    public void SetNickname(string nickname)
+    {
+        this.nickname = string.IsNullOrEmpty(nickname) ? "Unknown" : nickname;
+    }
 
     // 전투 시 필요한  my BattleUnit 생성
     // 전투에 사용할 특성 선택이 반영되므로 선택 이후에 호출하도록
-    public BattleUnit CreateBattleUnit(BattleSetupData setup)
+    private BattleUnit CreateBattleUnit(BattleSetupData setup, CharacterResponse characterData)
     {
-        float hp = characterStatusData.Hp;
-        float atk = characterStatusData.Atk;
-        float def = characterStatusData.Def;
-        float spd = characterStatusData.Spd;
-        float crtk = characterStatusData.Crit;
+        float hp = characterData.Hp;
+        float atk = characterData.Atk;
+        float def = characterData.Def;
+        float spd = characterData.Spd;
+        float crtk = characterData.Crit;
 
 
         int attrLevel = setup.selectedAttr switch
         {
-            AttributeType.Fire => characterStatusData.FireLv,
-            AttributeType.Water => characterStatusData.WaterLv,
-            AttributeType.Grass => characterStatusData.GrassLv,
-            AttributeType.Aurora => characterStatusData.AuroraLv,
+            AttributeType.Fire => characterData.FireLv,
+            AttributeType.Water => characterData.WaterLv,
+            AttributeType.Grass => characterData.GrassLv,
+            AttributeType.Aurora => characterData.AuroraLv,
             _ => 0
         };
 
@@ -71,9 +89,9 @@ public class CharacterManager : Singleton<CharacterManager>
             }
         }
 
-        if(setup.selectedEquips != null)
+        if(equippedItems != null)
         {
-            foreach(var equip in setup.selectedEquips)
+            foreach(var equip in equippedItems)
             {
                 if (equip == null) continue;
                 foreach(var bonus in equip.statBonuses)
@@ -85,7 +103,7 @@ public class CharacterManager : Singleton<CharacterManager>
 
         return new BattleUnit
         {
-            name = "Name",    // 이름 어디서 끌어오지 << 어디 저장하지? 이름?????
+            name = nickname,    // 이름 어디서 끌어오지 << 어디 저장하지? 이름????? (로그인 관리 스크립트에서 LoginResponse를 받아 저장한 후 받아오도록 해야할 것)
             maxHp = (int)hp,
             hp = (int)hp,
             atk = atk,
@@ -97,10 +115,22 @@ public class CharacterManager : Singleton<CharacterManager>
         };
     }
 
+    public async Task<BattleUnit> CreateBattleUnitAsync(BattleSetupData setup)
+    {
+        CharacterResponse characterData = await RefreshCharacterAsync();
+
+        return CreateBattleUnit(setup, characterData);
+    }
+
+    // BattleUIManager 컴파일 보호를 위한 임시 함수(로직 조정 후 삭제)
+    public BattleUnit CreateBattleUnit(BattleSetupData setup)
+    {
+        return CreateBattleUnit(setup, characterStatusData);
+    }
 
     // -------------------------- 장착 장비 처리 -------------------------------
     public void SetEquippedItems(List<EquipmentDataSO> items)
     {
-        equippedItems = items;
+        equippedItems = items != null ? new List<EquipmentDataSO>(items) : new List<EquipmentDataSO>();   // 복사 저장
     }
 }
