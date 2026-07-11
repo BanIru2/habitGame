0865f148dea40a13c749c37780f1e7d1c3e1982b
Bam2 <1004jangsan@gmail.com>
2026-07-11 18:08:13 +0900

    CharacterResponse update : align fields with API spec
    API 명세에 맞게 CharacterResponse 일부 필드명 수정

--- HabitGame/Assets/Scripts/Network/DTOs/Response/CharacterResponse.cs
+++ HabitGame/Assets/Scripts/Network/DTOs/Response/CharacterResponse.cs
@@ -13,6 +13,10 @@ public class CharacterResponse
     [JsonProperty("gold")]
     public int Gold { get; set; }
 
+    // 최대 체력
+    [JsonProperty("hp")]
+    public float Hp { get; set; }
+
     // 기본 공격력
     [JsonProperty("atk")]
     public float Atk { get; set; }
@@ -21,33 +25,13 @@ public class CharacterResponse
     [JsonProperty("def")]
     public float Def { get; set; }
 
-    // 최대 체력
-    [JsonProperty("hp")]
-    public float Hp { get; set; }
-
     // 속도 (턴 우선순위)
     [JsonProperty("spd")]
     public float Spd { get; set; }
 
     // 치명타 확률
-    [JsonProperty("crit_rate")]
+    [JsonProperty("critRate")]
     public float Crit { get; set; }
-
-    // 불(신체) 속성 경험치
-    [JsonProperty("fireExp")]
-    public int FireExp { get; set; }
-
-    // 물(바이오리듬) 속성 경험치
-    [JsonProperty("waterExp")]
-    public int WaterExp { get; set; }
-
-    // 풀(환경) 속성 경험치
-    [JsonProperty("grassExp")]
-    public int GrassExp { get; set; }
-
-    // 오로라(자기개발) 속성 경험치
-    [JsonProperty("auroraExp")]
-    public int AuroraExp { get; set; }
 
     // 불(신체) 속성 레벨
     [JsonProperty("fireLv")]
@@ -64,4 +48,20 @@ public class CharacterResponse
     // 오로라(자기개발) 속성 레벨
     [JsonProperty("auroraLv")]
     public int AuroraLv { get; set; }
+
+    // 불(신체) 속성 경험치
+    [JsonProperty("fireExp")]
+    public int FireExp { get; set; }
+
+    // 물(바이오리듬) 속성 경험치
+    [JsonProperty("waterExp")]
+    public int WaterExp { get; set; }
+
+    // 풀(환경) 속성 경험치
+    [JsonProperty("grassExp")]
+    public int GrassExp { get; set; }
+
+    // 오로라(자기개발) 속성 경험치
+    [JsonProperty("auroraExp")]
+    public int AuroraExp { get; set; }
 }
